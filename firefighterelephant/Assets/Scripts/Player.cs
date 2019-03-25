2618577495421b94c44680f92277237ffd33878f
Lucas Schneider Postai <0177766@feevale.br>
2019-03-25 19:11:49 -0300

    Player Movement

---
+++ firefighterelephant/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMovement))]
+public class Player : MonoBehaviour
+{
+	public float MoveSpeed;
+
+	private Vector3 velocity;
+	private float gravity = -20f;
+	private PlayerMovement movement;
+	private Vector3 input;
+
+	private void Start()
+	{
+		movement = GetComponent<PlayerMovement>();
+	}
+
+	private void Update()
+	{
+		input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+		velocity.x = input.x * MoveSpeed;
+		velocity.y += gravity * Time.deltaTime;
+		movement.Move(velocity * Time.deltaTime);
+	}
+}
